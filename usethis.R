2bc2291a2140aa01b6fdd3ba3337de8480e418b2
Daniel795-lab <diugalde@uc.cl>
2020-08-09 02:54:21 -0400

    Se agregan funciones del paquete usethis

--- usethis.R
+++ usethis.R
@@ -6,3 +6,6 @@ usethis::edit_r_environ()
 
 use_git() #activar
 usethis::use_github() #y conectar el proyecto a GitHub
+
+usethis::pr_pull_upstream()#bajo o descargo los cambios que hizo el colaborador
+usethis::pr_init("sitio_web")#crea rama denominada "sitio_web"
