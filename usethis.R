6edfaf58d6ae803718623709ebdd4193cb483e4f
Daniel795-lab <diugalde@uc.cl>
2020-08-10 14:40:39 -0400

    modificaciones en usethis

--- usethis.R
+++ usethis.R
@@ -7,11 +7,10 @@ usethis::edit_r_environ()
 use_git() #activar
 usethis::use_github() #y conectar el proyecto a GitHub
 
-usethis::pr_pull_upstream()#bajo o descargo los cambios que hizo el colaborador
+usethis::pr_pull_upstream()# descargo los archivos "nuevos" del upstream para actualizar mi repositorio local
 usethis::pr_init("sitio_web")#crea rama denominada "sitio_web"
 devtools::build_site()#genera un .html en la carpeta docs
 usethis::pr_push()#envio los cambios, se realiza un pull request a mi mismo.
-usethis::pr_pull_upstream()
 
 #Crea README
-usethis::use_readme_rmd()
+usethis::use_readme_rmd()#solo funciona cuando se crean nuevos paquetes
